3e17164dda0b89df64d305731f658e4007af5c1a
Peter Ahlgren <ahlgren1234@gmail.com>
2022-02-04 15:22:45 +0100

    Initial Commit

---
+++ TipCalc/TipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcApp.swift
+//  TipCalc
+//
+//  Created by Peter Ahlgren on 2022-02-04.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
