2266547fb07f6f0cc23428c838d2a603bb917465
Peter Ahlgren <ahlgren1234@gmail.com>
2022-02-04 16:27:06 +0100

    Add GuestCountView

---
+++ TipCalc/GuestCountView.swift
@@ -0,0 +1,48 @@
+//
+//  GuestCountView.swift
+//  TipCalc
+//
+//  Created by Peter Ahlgren on 2022-02-04.
+//
+
+import SwiftUI
+
+struct GuestCountView: View {
+    @Binding var guestCount: Int
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Button {
+                if guestCount > 1 {
+                    guestCount -= 1
+                }
+            } label: {
+                Image(systemName: "minus.circle")
+                    .foregroundColor(.indigo)
+                    .font(.system(.title, design: .rounded))
+            }
+            
+            Text("\(guestCount)")
+                .foregroundColor(.primary)
+                .font(.system(size: 40, weight: .black, design: .monospaced))
+            
+            Button {
+                guestCount += 1
+            } label: {
+                Image(systemName: "plus.circle")
+                    .foregroundColor(.indigo)
+                    .font(.system(.title, design: .rounded))
+            }
+        }
+    }
+}
+
+struct GuestCountView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            GuestCountView(guestCount: .constant(1))
+                
+            
+        }
+    }
+}
